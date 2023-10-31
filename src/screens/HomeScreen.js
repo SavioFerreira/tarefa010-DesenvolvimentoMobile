e23641eae7b410e9258214fde9af60739748cdeb
Lucas Nunes Carvalho <lucasnc10@hotmail.com>
2023-10-31 19:55:50 -0300

    alteração select da pagina HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -10,20 +10,10 @@ const HomeScreen = ({ navigation }) => {
   useEffect(() => {
     db.transaction(function (txn) {
       txn.executeSql(
-        "SELECT name FROM sqlite_master WHERE type='table' AND name='table_user'",
-        [],
-        function (tx, res) {
-          console.log('item:', res.rows.length);
-          if (res.rows.length == 0) {
-            txn.executeSql('DROP TABLE IF EXISTS table_user', []);
-            txn.executeSql(
-              'CREATE TABLE IF NOT EXISTS table_user(user_id INTEGER PRIMARY KEY, user_name VARCHAR(20), user_date VARCHAR(20), user_email VARCHAR(30))',
-              []
-            );
-          }
-        }
+        'CREATE TABLE IF NOT EXISTS table_user(user_id INTEGER PRIMARY KEY, user_name VARCHAR(20), user_date VARCHAR(20), user_email VARCHAR(30))',
+        []
       );
-    });
+  })
   }, []);
 
   return (
